57cbda74498d5c0b6ea020f72075c3fe23f1afad
HuydDo <huydinhdo@yahoo.com>
2020-02-10 22:10:39 -0700

    Done.

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,11 +1,8 @@
 class TrainersController < ApplicationController
   def index
     @trainers = Trainer.all
-    # byebug
-    trainerArr = @trainers.map( trainer => TrainerSerializer.new(trainer))
-    # trainerArr = @trainers.map{|trainer| TrainerSerializer.new(trainer)}
-    render json: trainerArr
-      
+    render json: @trainers
   end
 
+  
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -3,19 +3,30 @@ const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 const mainDiv = document.querySelector('main')
 
+let pokemons =[]
+fetch(POKEMONS_URL)
+  .then(res => res.json())
+  .then(json => pokemons = json)
 
 fetch(TRAINERS_URL)
   .then(res => res.json())
-  .then(putTrainersOnPage)
+  // .then(putTrainersOnPage)
   // .then(json => console.log(json))
+  .then(json => putTrainersOnPage(json))
   
 
 function putTrainersOnPage(trainers){
-  // console.log(trainers)
+  // console.log(pokemons)
   trainers.forEach(trainer => {
-  console.log(trainer)
+  // console.log(trainer)
     let pokString = ""
-    trainer.pokemons.forEach(pokemon => {
+    trainer_pokemons = pokemons.filter(pokemon => {
+      // console.log(trainer.id)
+      // console.log(pokemon.trainer_id)
+      return trainer.id === pokemon.trainer_id})
+
+    // console.log(trainer_pokemons)
+    trainer_pokemons.forEach(pokemon => {
       pokString += `<li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>`
     })
     mainDiv.innerHTML += `
@@ -37,8 +48,20 @@ function putTrainersOnPage(trainers){
           trainer_id: e.target.dataset.trainerId
         })
       })
-        .then(res => res.json())
-        .then(addPokemon)
+        // .then(res => res.json())
+        .then(res => {
+          console.log(`status: ${res.status}`);
+          console.dir(res.body);
+          return res.json()
+        })
+        // .then(addPokemon)
+
+        // .then(json => console.log(json))
+
+        .then(myJson => addPokemon(myJson))
+        // .catch(function (error) {
+        //   console.log('Request failed', error);
+        // });
     }
     if (e.target.dataset.pokemonId !== undefined){
       e.target.parentElement.remove()
@@ -47,6 +70,7 @@ function putTrainersOnPage(trainers){
   })
 
   function addPokemon(pokemon){
+    console.log(pokemon)
     mainDiv.children[pokemon.trainer_id-1].lastElementChild.innerHTML += `<li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>`
   }
 
